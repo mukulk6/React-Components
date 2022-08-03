18d565d95563da5aa9518717e0835aea1951eedb
mukul <mukul.kale@digitalmain.com>
2022-08-03 16:13:02 +0530

    Multiselect dropwdown using react bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import SeekerData from '../src/components/Elements/getprofileSeeker.json';
 import Elements from './components/Elements/Elements';
 import TabSearch from './components/TabSearch/TabSearch';
+import Multiselectdropdowndomain from './components/Multiselectdropdowndomain/Multiselectdropdowndomain';
 
 function App() {
   return (
     <div className="App">
       <Elements />
       <TabSearch />
+      <Multiselectdropdowndomain />
     </div>
   );
 }
---
+++ src/components/Multiselectdropdowndomain/Multiselectdropdowndomain.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import { Form, Col } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import styles from './Multiselectdropdowndomain.module.css';
+
+function Multiselectdropdowndomain() {
+  const [field, setField] = useState([]);
+
+  return (
+    <Form.Group as={Col} controlId="my_multiselect_field">
+      <Form.Label>My multiselect</Form.Label>
+      <Form.Control as="select" multiple value={field} onChange={e => setField([].slice.call(e.target.selectedOptions).map(item => item.value))}>
+        <option value="field1">Field 1</option>
+        <option value="field2">Field 2</option>
+        <option value="field3">Field 3</option>
+      </Form.Control>
+    </Form.Group>
+  );
+}
+
+Multiselectdropdowndomain.propTypes = {};
+
+Multiselectdropdowndomain.defaultProps = {};
+
+export default Multiselectdropdowndomain;
