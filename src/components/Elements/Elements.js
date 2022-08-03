813c9b3a074d590401031a5e3bca1d3d38ffa59c
mukul <mukul.kale@digitalmain.com>
2022-08-03 11:04:42 +0530

    First compoennt created

---
+++ src/components/Elements/Elements.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Elements.module.css';
+
+const Elements = () => (
+  <div className={styles.Elements}>
+    Elements Component
+  </div>
+);
+
+Elements.propTypes = {};
+
+Elements.defaultProps = {};
+
+export default Elements;
