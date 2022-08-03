a3e7c42239f2782410322963fb2e89c2dbb43634
mukul <mukul.kale@digitalmain.com>
2022-08-03 12:27:54 +0530

    Sort By filter added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import SeekerData from '../src/components/Elements/getprofileSeeker.json';
+import Elements from './components/Elements/Elements';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Elements />
     </div>
   );
 }
