2037f8c0d1b6c43ebb759574a853625ca91efec5
mukul <mukul.kale@digitalmain.com>
2022-08-03 11:34:46 +0530

    Added bootstrap css and js files

--- src/components/Elements/Elements.js
+++ src/components/Elements/Elements.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Elements.module.css';
+import SeekerProfile from './getprofileSeeker.json';
 
 const Elements = () => (
-  <div className={styles.Elements}>
-    Elements Component
-  </div>
+  <div class="col-md-3">
+  <span id={styles.sortBy}>Sort by:</span>
+  <select class="form-select" className={styles.sortDropbox} aria-label="Default select example">
+    <option selected>Recently Answered</option>
+    <option value="1">Oldest (Answered)</option>
+    <option value="2">Recently Asked</option>
+    <option value="3">Oldest asked</option>
+    <option value="4">Most Viewed</option>
+  </select>
+</div>
 );
 
 Elements.propTypes = {};
