f9812b113a1baf9b7997903839de1cf7fff53901
mukul <mukul.kale@digitalmain.com>
2022-08-11 20:37:42 +0530

    AI toggle css

--- src/components/AiToggle/AiToggle.js
+++ src/components/AiToggle/AiToggle.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './AiToggle.module.css';
-import {Form} from 'react-bootstrap';
+import {Form, Button, FormLabel} from 'react-bootstrap';
 
 const AiToggle = () => (
   <div class="row">
     <div class="col-md-2">
       <span>
+        <Button as={FormLabel} size="sm" id={styles.btnToggle}>
         <Form>
           <Form.Check
             type="switch"
-            id="custom-switch"
-            label="ON"
-            title="ON"
+            id={styles.customswitch}
           />
         </Form>
+        </Button>
       </span>
     </div>
   </div>
