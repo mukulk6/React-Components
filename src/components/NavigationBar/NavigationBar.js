f9725dcebfea53e3cceb52ad8ef1b6fb561c6105
mukul <mukul.kale@digitalmain.com>
2022-08-12 10:34:51 +0530

    Navigation bar component added

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './NavigationBar.module.css';
+
+const NavigationBar = () => (
+  <div className={styles.NavigationBar}>
+    NavigationBar Component
+  </div>
+);
+
+NavigationBar.propTypes = {};
+
+NavigationBar.defaultProps = {};
+
+export default NavigationBar;
