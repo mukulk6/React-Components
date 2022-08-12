7d38aaec4dcaf53767c2631b2cabd1461f61e2bf
mukul <mukul.kale@digitalmain.com>
2022-08-12 13:26:29 +0530

    Wrapper searchbar component

---
+++ src/components/Wrappersearchbar/Wrappersearchbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Wrappersearchbar.module.css';
+
+const Wrappersearchbar = () => (
+  <div className={styles.Wrappersearchbar}>
+    <div className={styles.midContainer}>
+      <div class="container">
+      </div>
+    </div>
+  </div>
+);
+
+Wrappersearchbar.propTypes = {};
+
+Wrappersearchbar.defaultProps = {};
+
+export default Wrappersearchbar;
