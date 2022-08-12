4943e25be935fc7f283fe9ce5701f33ad9404c68
mukul <mukul.kale@digitalmain.com>
2022-08-12 12:48:42 +0530

    Open tab component Seeker

---
+++ src/components/OpentabSeeker/OpentabSeeker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OpentabSeeker.module.css';
+
+const OpentabSeeker = () => (
+  <div className={styles.OpentabSeeker}>
+    OpentabSeeker Component
+  </div>
+);
+
+OpentabSeeker.propTypes = {};
+
+OpentabSeeker.defaultProps = {};
+
+export default OpentabSeeker;
