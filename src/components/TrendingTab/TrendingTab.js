64b636c739bab71a52f9b4e2208196cd659f4674
mukul <mukul.kale@digitalmain.com>
2022-08-18 13:00:49 +0530

    Trending tab code changes

--- src/components/TrendingTab/TrendingTab.js
+++ src/components/TrendingTab/TrendingTab.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types';
 import styles from './TrendingTab.module.css';
 import NavigationBar from '../NavigationBar/NavigationBar';
 import SearchBar from '../SearchBar/SearchBar';
+import SeekerData from '../Elements/getprofileSeeker.json';
+import TabSearch from '../TabSearch/TabSearch';
+import Elements from '../Elements/Elements';
+import Multiselectdropdowndomain from '../Multiselectdropdowndomain/Multiselectdropdowndomain';
+
 
 const TrendingTab = () => (
 
@@ -23,6 +28,25 @@ const TrendingTab = () => (
         </div>
       </div>
     </div>
+    <section>
+    <div class="row" id={styles.bottomRow}>
+          <div class="col-md-3">
+            <h3 className={styles.openQuestions}>Trending Questions</h3>
+          </div>
+          <TabSearch />
+          <Elements />
+          {
+            SeekerData.map((postData, index) => {
+              return (
+                <div key={index} class="col-md-3">
+                  <span id={styles.domainTextMultiselect}>Domain:</span>
+                  <Multiselectdropdowndomain data={postData} />
+                </div>
+              )
+            })
+          }
+        </div>
+    </section>
   </div>
 );
 
