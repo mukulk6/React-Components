c71a2d425527da55c6c1850c5a5244cb3ec562a8
mukul <mukul.kale@digitalmain.com>
2022-08-18 11:43:50 +0530

    Trending tab component added

---
+++ src/components/TrendingTab/TrendingTab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './TrendingTab.module.css';
+
+const TrendingTab = () => (
+  <div className={styles.TrendingTab}>
+    TrendingTab Component
+  </div>
+);
+
+TrendingTab.propTypes = {};
+
+TrendingTab.defaultProps = {};
+
+export default TrendingTab;
