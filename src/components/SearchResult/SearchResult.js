3ae199de9d03f5070227a222650920d10935c4b8
mukul <mukul.kale@digitalmain.com>
2022-08-19 11:09:09 +0530

    Search Result component added

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './SearchResult.module.css';
+
+const SearchResult = () => (
+  <div className={styles.SearchResult}>
+    SearchResult Component
+  </div>
+);
+
+SearchResult.propTypes = {};
+
+SearchResult.defaultProps = {};
+
+export default SearchResult;
