24ce5ca6fe0f010c4f5b8b8d517e294a1e77ef26
mukul <mukul.kale@digitalmain.com>
2022-08-22 12:48:20 +0530

    Toggle switch componenet

---
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ToggleSwitch.module.css';
+
+const ToggleSwitch = () => (
+  <div className={styles.ToggleSwitch}>
+    ToggleSwitch Component
+  </div>
+);
+
+ToggleSwitch.propTypes = {};
+
+ToggleSwitch.defaultProps = {};
+
+export default ToggleSwitch;
