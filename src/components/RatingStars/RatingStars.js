5798c77cfab136caaccd68a23e104a8335162fa2
mukul <mukul.kale@digitalmain.com>
2022-09-22 15:54:02 +0530

    Changes in show more

--- src/components/RatingStars/RatingStars.js
+++ src/components/RatingStars/RatingStars.js
@@ -4,8 +4,33 @@ import styles from './RatingStars.module.css';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 
-const RatingStars = () => { 
+// const useStyles = makeStyles({
+//   root: {
+//     width: 200,
+//     display: "flex",
+//     alignItems: "center"
+//   },
+//   iconFilled: {},
+//   iconFilled1: { color: "red" },
+//   iconFilled2: { color: "orange" },
+//   iconFilled3: { color: "yellow" },
+//   iconFilled4: { color: "blue" },
+//   iconFilled5: { color: "green" },
+//   iconHover: {},
+//   iconHover1: { color: "red" },
+//   iconHover2: { color: "orange" },
+//   iconHover3: { color: "yellow" },
+//   iconHover4: { color: "blue" },
+//   iconHover5: { color: "green" }
+// });
+
+const RatingStars = () => {
+  // const classes = useStyles();
   const [value, setValue] = React.useState(2);
+  // const [iconFilledVar, setIconFilled] = React.useState(classes.iconFilled);
+  // const [iconHoverVar, setIconHover] = React.useState(classes.iconHover);
+
+
   return (
   <div className={styles.ratingStarsInput}>
     <Rating
@@ -15,8 +40,57 @@ const RatingStars = () => {
       sx={{marginBottom:'15px',}}
       onChange={(event, newValue) => {
         setValue(newValue);
-      }
-    }
+        // switch (true) {
+        //   case newValue <= 1: {
+        //     setIconFilled(classes.iconFilled1);
+        //     break;
+        //   }
+        //   case newValue <= 2 && newValue > 1: {
+        //     setIconFilled(classes.iconFilled2);
+        //     break;
+        //   }
+        //   case newValue <= 3 && newValue > 2: {
+        //     setIconFilled(classes.iconFilled3);
+        //     break;
+        //   }
+        //   case newValue <= 4 && newValue > 3: {
+        //     setIconFilled(classes.iconFilled4);
+        //     break;
+        //   }
+        //   case newValue > 4: {
+        //     setIconFilled(classes.iconFilled5);
+        //     break;
+        //   }
+        // }
+      }}
+      // onChangeActive={(event, newHover) => {
+      //   switch (true) {
+      //     case newHover <= 1: {
+      //       setIconHover(classes.iconHover1);
+      //       break;
+      //     }
+      //     case newHover <= 2 && newHover > 1: {
+      //       setIconHover(classes.iconHover2);
+      //       break;
+      //     }
+      //     case newHover <= 3 && newHover > 2: {
+      //       setIconHover(classes.iconHover3);
+      //       break;
+      //     }
+      //     case newHover <= 4 && newHover > 3: {
+      //       setIconHover(classes.iconHover4);
+      //       break;
+      //     }
+      //     case newHover > 4: {
+      //       setIconHover(classes.iconHover5);
+      //       break;
+      //     }
+      //   }
+      // }}
+      // classes={{
+      //   iconFilled: iconFilledVar,
+      //   iconHover: iconHoverVar
+      // }}
     />
   </div>
 )
