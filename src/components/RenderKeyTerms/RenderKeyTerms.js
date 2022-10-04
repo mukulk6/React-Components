3efc29a7e1189a6c94a1a248a3febaf9d67d02c1
mukul <mukul.kale@digitalmain.com>
2022-10-04 12:47:19 +0530

    Render Key Terms component added

---
+++ src/components/RenderKeyTerms/RenderKeyTerms.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './RenderKeyTerms.module.css';
+
+const RenderKeyTerms = () => (
+  <div className={styles.RenderKeyTerms}>
+    RenderKeyTerms Component
+  </div>
+);
+
+RenderKeyTerms.propTypes = {};
+
+RenderKeyTerms.defaultProps = {};
+
+export default RenderKeyTerms;
