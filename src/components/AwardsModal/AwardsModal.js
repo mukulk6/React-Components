3106f83dc0bda2e312e1e9af9f56b884337482f4
mukul <mukul.kale@digitalmain.com>
2022-10-06 11:20:06 +0530

    Awards modal component

---
+++ src/components/AwardsModal/AwardsModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './AwardsModal.module.css';
+
+const AwardsModal = () => (
+  <div className={styles.AwardsModal}>
+    AwardsModal Component
+  </div>
+);
+
+AwardsModal.propTypes = {};
+
+AwardsModal.defaultProps = {};
+
+export default AwardsModal;
