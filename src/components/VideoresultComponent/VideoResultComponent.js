c1bfe26162cc03e0fea6e2549423593bf2890b0c
mukul <mukul.kale@digitalmain.com>
2022-11-01 16:21:17 +0530

    Video result componenet added

---
+++ src/components/VideoresultComponent/VideoResultComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './VideoresultComponent.module.css';
+
+const VideoResultComponent = () => (
+  <div className={styles.VideoresultComponent}>
+    VideoresultComponent Component
+  </div>
+);
+
+VideoResultComponent.propTypes = {};
+
+VideoResultComponent.defaultProps = {};
+
+export default VideoResultComponent;
