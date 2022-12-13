811f1358edbc433af5b0d3ee17d1d2d61a33d9bc
mukul <mukul.kale@digitalmain.com>
2022-12-13 10:40:28 +0530

    CSS changes for Key term component

--- src/components/KeyTermsInnerSection/KeyTermsInnerSection.js
+++ src/components/KeyTermsInnerSection/KeyTermsInnerSection.js
@@ -38,24 +38,26 @@ const KeyTermsInnerSection = () => {
               <h3 className={styles.keyTermName}>standard lead time</h3>
               <div className={styles.keyTermCount}>1/1</div>
             </div>
-            <div class="col-md-7 float-end">
-              <div class="row">
+            <div class="col-md-8">
+              <div class="row justify-content-end" style={{ paddingTop: '12px' }}>
                 <div class="col-md-4">
-                <TabSearch />
-              </div>  
-              <span>View By:</span>
-              <Form.Select aria-label="Default select example" size='sm' className={styles.selectKeyTerm}>
-                <option value="1">Expert Q/A</option>
-                <option value="2">User Queries</option>
-              </Form.Select>
+                  <TabSearch />
+                </div>
+                <div class="col-md-4">
+                  <span className={styles.viewByText}>View By:</span>
+                  <Form.Select aria-label="Default select example" size='sm' className={styles.selectKeyTerm}>
+                    <option value="1">Expert Q/A</option>
+                    <option value="2">User Queries</option>
+                  </Form.Select>
+                </div>
               </div>
             </div>
           </div >
           {
-            KeyTermData.map((data,index)=>{
-              return(
-                <div key={index} style={{paddingTop:'20px'}}>
-                  <ReusableCardComponent data={data}/>
+            KeyTermData.map((data, index) => {
+              return (
+                <div key={index} style={{ paddingTop: '20px' }}>
+                  <ReusableCardComponent data={data} />
                 </div>
               )
             })
